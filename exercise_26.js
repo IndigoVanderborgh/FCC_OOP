ab7790a8212c473013705acd825d85cd75d3cef0
IndigoVanderborgh <91171405+IndigoVanderborgh@users.noreply.github.com>
2024-02-28 19:42:56 -0800

    Update exercise_26.js

--- exercise_26.js
+++ exercise_26.js
@@ -39,16 +39,20 @@
 // ------------------------------------------ exercise ------------------------------------------------------------------------------------------------------------------------------
 // Create a module named funModule to wrap the two mixins isCuteMixin and singMixin. funModule should return an object.
 
-let isCuteMixin = function(obj) {
-    obj.isCute = function() {
-        return true;
+let funModule = (function() {
+    return {
+        isCuteMixin: function(obj) {
+            obj.isCute = function() {
+                return true;
+            };
+        },
+    singMixin: function(obj) {
+        obj.sing = function() {
+            console.log("Singing to an awesome tune");
+            };
+        }
     };
-};
-let singMixin = function(obj) {
-    obj.sing = function() {
-        console.log("Singing to an awesome tune");
-    };
-};
+})();
 
 // let isCuteMixin = function(obj) {
 //     obj.isCute = function() {
